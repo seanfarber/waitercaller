0054ca46e313a3beb846288106ca02afe931f604
Sean Farber <seanfarber@users.noreply.github.com>
2019-03-08 11:41:01 -0500

    fix

--- waitercaller.py
+++ waitercaller.py
@@ -108,7 +108,7 @@ def account_createtable():
     form = CreateTableForm(request.form)
     if form.validate():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
-        new_url = config.base_url + "newrequest/" + str(tableid)
+        new_url = config.base_url + "/newrequest/" + str(tableid)
         DB.update_table(tableid, new_url)
         return redirect(url_for('account'))
     return render_template('account.html', createtableform=form, tables=DB.get_tables(current_user.get_id()))
